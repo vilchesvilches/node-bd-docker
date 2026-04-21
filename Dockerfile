2d40f1a49dd264f6179478e1d5fc22e16a65d710
Cristian <cchura.cpc@gmail.com>
2026-04-20 22:22:18 -0400

    add:configuraciones extras docker

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:22
+FROM node:22-alpine3.20
 
 WORKDIR /app
 
@@ -10,4 +10,5 @@ COPY . .
 
 EXPOSE 3000
 
+# cuando ejecutas docker run
 CMD ["node", "src/index.js"]
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 const express = require("express");
+require("dotenv").config();
+
 const User = require("./models/User");
 const Producto = require("./models/Producto");
 // inicializando una app de express
 const app = express();
 
+// crear variables
+const port = process.env.PORT || 3000;
+
 app.get("/", function(req, res){
     return res.json({mensaje: "Saludos desde mi App de Node (MODIFICADO) último 3"});
 });
 
-app.listen(3000, function(){
+app.listen(port, function(){
     console.log("Servidor corriendo en el puerto 3000 (ultima modificación) 3");
 })
